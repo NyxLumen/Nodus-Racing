451a7752f72f6005c45a57493ec99eb98fdf62b0
NyxLumen <169152457+NyxLumen@users.noreply.github.com>
2026-03-31 14:22:59 +0530

    asset managment and updated the hero section to follow new deployement tactics

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 // src/components/Hero.jsx
 export default function Hero() {
 	return (
-		<section className="relative flex-grow flex items-center justify-center w-full overflow-hidden">
+		<section className="relative grow flex items-center justify-center w-full overflow-hidden">
 			<div className="relative z-10 flex flex-col items-center justify-center text-center text-lavender-mist w-full -translate-y-[8vh]">
 				{/* Added font-anton here to protect the heavy styling */}
 				<h1 className="font-anton text-[22vw] tracking-wider leading-none m-0 uppercase drop-shadow-lg">
@@ -16,7 +16,7 @@ export default function Hero() {
 
 			<div className="absolute inset-0 z-20 flex items-center justify-center pointer-events-none translate-y-[16vh] mix-blend-luminosity">
 				<img
-					src="/car.png"
+					src="/pictures/car.png"
 					alt="Nodus Racing"
 					className="w-[105%] max-w-[100rem] object-contain drop-shadow-2xl"
 				/>
